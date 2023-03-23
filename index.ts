b478b30d89f6522630260dd5a20cf69ee8e92ef8
Isak Cruz <isakcruzcs@gmail.com>
2023-03-23 17:03:40 -0300

    adicionando solucoes das questoes 01 e 02 em index.ts

---
+++ index.ts
@@ -0,0 +1,43 @@
+//questão01
+let employee : {code : number, name: string} = {code:0, name:""};
+
+employee.code = 10;
+
+employee.name = "John";
+
+//questão02
+type Pessoa = { nome : string, idade: number, profissao: string };
+
+let pessoa1 : Pessoa = {
+    nome : "maria",
+
+    idade : 29,
+
+    profissao : "atriz"
+};
+
+let pessoa2 = {
+    nome : "roberto",
+
+    idade : 19,
+
+    profissao : "Padeiro"
+}
+
+let pessoa3 = {
+    nome: "laura",
+
+    idade: 32,
+
+    profissao: "Atriz"
+
+};
+
+let pessoa4 = {
+    nome : "carlos",
+
+    idade : 19,
+
+    profissao : "padeiro"
+
+}
